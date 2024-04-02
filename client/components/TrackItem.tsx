6659a0b1a96e054619dd09b40cb2ec1837ce3ce1
nuwa37 <yond41m3@icloud.com>
2024-04-02 17:34:31 +0500

    Modified track item

--- client/components/TrackItem.tsx
+++ client/components/TrackItem.tsx
@@ -1,5 +1,8 @@
-import { Card } from '@material-ui/core';
+import { Card, Grid, IconButton } from '@material-ui/core';
+import { Delete, Pause, PlayArrow } from '@material-ui/icons';
+import { useRouter } from 'next/navigation';
 import { FC } from 'react';
+import styles from '../styles/TrackItem.module.scss';
 import { Track } from '../types/track';
 
 interface TrackItemProps {
@@ -8,7 +11,26 @@ interface TrackItemProps {
 }
 
 const TrackItem: FC<TrackItemProps> = ({ track, active = false }) => {
-  return <Card>{track.name}</Card>;
+  const router = useRouter();
+
+  const onCardClickHandler = () => {
+    router.push('/tracks/' + track._id);
+  };
+
+  return (
+    <Card className={styles.track} onClick={onCardClickHandler}>
+      <IconButton>{active ? <Pause /> : <PlayArrow />}</IconButton>
+      <img width={70} height={70} src={track.picture} />
+      <Grid container direction='column' className={styles.info}>
+        <h4>{track.name}</h4>
+        <h5>{track.artist}</h5>
+      </Grid>
+      {active && <div>02:24 / 03:56</div>}
+      <IconButton className={styles.deleteBtn}>
+        <Delete />
+      </IconButton>
+    </Card>
+  );
 };
 
 export default TrackItem;
