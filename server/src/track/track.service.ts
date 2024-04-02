02010f4ee652d744b2aa0b744f4fd076da7efcb3
nuwa37 <yond41m3@icloud.com>
2024-04-02 10:56:30 +0500

    Modified track service and controller

--- server/src/track/track.service.ts
+++ server/src/track/track.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
+import { CreateCommentDto } from './dto/create.comment.dto';
 import { CreateTrackDto } from './dto/create.track.dto';
 import { Comment, CommentDocument } from './schemas/comment.schema';
 import { Track, TrackDocument } from './schemas/track.schema';
@@ -17,9 +18,28 @@ export class TrackService {
     return track;
   }
 
-  async getAll() {}
+  async getAll(): Promise<Track[]> {
+    const tracks = await this.trackModel.find();
+    return tracks;
+  }
 
-  async getOne() {}
+  async getOne(id: ObjectId): Promise<Track> {
+    const track = await this.trackModel.findById(id).populate('comments');
+    return track;
+  }
 
-  async delete() {}
+  async delete(id: ObjectId): Promise<ObjectId> {
+    const track = await this.trackModel.findByIdAndDelete(id);
+    return track._id;
+  }
+
+  async addComment(dto: CreateCommentDto): Promise<Comment> {
+    const track = await this.trackModel.findById(dto.trackId);
+    const comment = await this.commentModel.create({ ...dto });
+
+    track.comments.push(comment._id);
+    await track.save();
+
+    return comment;
+  }
 }
