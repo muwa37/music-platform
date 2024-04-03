ff86c9042e0176c5ca52a828578ade0286e09d9c
nuwa37 <yond41m3@icloud.com>
2024-04-03 16:37:54 +0500

    Added redux

---
+++ client/store/reducers/index.ts
@@ -0,0 +1,20 @@
+import { HYDRATE } from 'next-redux-wrapper';
+import { combineReducers } from 'redux';
+import { playerReducer } from './playerReducer';
+
+const rootReducer = combineReducers({ player: playerReducer });
+
+export const reducer = (state, action) => {
+  if (action.type === HYDRATE) {
+    const nextState = {
+      ...state,
+      ...action.payload,
+    };
+    if (state.count) nextState.count = state.count;
+    return nextState;
+  } else {
+    return rootReducer(state, action);
+  }
+};
+
+export type RootState = ReturnType<typeof rootReducer>;
