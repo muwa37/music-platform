c2cc4264b7f8f64d0d452e64fd90ae8bc098c2e6
nuwa37 <yond41m3@icloud.com>
2024-04-04 18:24:47 +0500

    Added track posting

--- client/pages/tracks/create.tsx
+++ client/pages/tracks/create.tsx
@@ -1,19 +1,42 @@
 import { Button, Grid, TextField } from '@material-ui/core';
+import axios from 'axios';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 import FileUpload from '../../components/FileUpload';
 import StepWrapper from '../../components/StepWrapper';
+import { useInput } from '../../hooks/useIntput';
 import MainLayout from '../../layouts/MainLayout';
 
 const Create = () => {
+  const router = useRouter();
+
   const [activeStep, setActiveStep] = useState(0);
   const [cover, setCover] = useState(null);
   const [audio, setAudio] = useState(null);
+
+  const name = useInput('');
+  const artist = useInput('');
+  const text = useInput('');
 
   const onPrevStepClickHandler = () => {
     setActiveStep(prev => prev - 1);
   };
   const onNextStepClickHandler = () => {
     setActiveStep(prev => prev + 1);
+    if (activeStep === 2) {
+      const formData = new FormData();
+
+      formData.append('name', name.value);
+      formData.append('artist', artist.value);
+      formData.append('text', text.value);
+      formData.append('picture', cover);
+      formData.append('audio', audio);
+
+      axios
+        .post('http://localhost:5000/tracks', formData)
+        .then(res => router.push('tracks'))
+        .catch(e => console.log(e));
+    }
   };
 
   return (
@@ -21,9 +44,10 @@ const Create = () => {
       <StepWrapper activeStep={activeStep}>
         {activeStep === 0 && (
           <Grid container direction='column' style={{ padding: 20 }}>
-            <TextField style={{ marginTop: 10 }} label='track name' />
-            <TextField style={{ marginTop: 10 }} label='artist' />
+            <TextField {...name} style={{ marginTop: 10 }} label='track name' />
+            <TextField {...artist} style={{ marginTop: 10 }} label='artist' />
             <TextField
+              {...text}
               style={{ marginTop: 10 }}
               label='song text'
               multiline
